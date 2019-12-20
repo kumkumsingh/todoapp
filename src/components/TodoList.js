df764630bf5f49badf579c7f6dd901dd5239b906
kumkumsingh <kumkumsingh.r@gmail.com>
2019-12-20 23:53:53 +0100

    added edit functionality

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,51 +1,75 @@
 import React, { useState } from "react";
-import { TodoState } from "../contexts/Todo";
+import  { TodoState } from "../contexts/Todo";
 // Create a shorthand Hook for using the GlobalState
 const useTodoState = () => React.useContext(TodoState);
 
 export default function TodoList() {
-    const todos = useTodoState();
-    const [newTodo, setNewTodo] = useState({ title: '' })
+  const todos = useTodoState();
+  const [newTodo, setNewTodo] = useState({ title: "" });
   // Create a function which mutates GlobalState
-  function updateNewTodo(e) {
+  function addNewTodo(e) {
     console.log("checking adding task", e.target.value);
-    setNewTodo({ title: e.target.value })
-}
+    setNewTodo({ title: e.target.value });
+  }
 
-function handleSubmit(event){
-    console.log('submitting')
+  function handleSubmit(event) {
+    console.log("submitting");
     // console.log("checking global state", TodoState.set({ count: 5 }));
 
     TodoState.set({
-        todos: [...todos, { ...newTodo, id: todos.length + 1}]
+      todos: [...todos, { ...newTodo, id: todos.length + 1 }]
     });
 
-    setNewTodo({ title: ''})
-}
+    setNewTodo({ title: "" });
+  }
 
-function deleteTodo(id){
+  function deleteTodo(id) {
     TodoState.set({
-        todos: todos.filter(todo => todo.id !== id)
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+  function updateTodo(value, id) {
+    todos.map((todo) => { 
+         if(todo.id === id) {
+        
+         todo.title = value;
+         console.log('checking todo value',todo.title + "  id  " + todo.id);
+        }
+        return todo;
+    });
+    TodoState.set({
+        todos:todos
     })
-}
-  console.log(newTodo)
+
+    console.log("checking edited todo", );
+    //setNewTodo({ title: e.target.value });
+  }
+  console.log(newTodo);
   return (
     <div>
       <div>Todos</div>
       <input
         placeholder=" Add a Task"
         value={newTodo.title}
-        name="task"
-        onChange={updateNewTodo}
+        name="title"
+        onChange={addNewTodo}
       ></input>
       <button type="submit" onClick={handleSubmit}>
         Add Todo
       </button>
-      { todos.map(todo => {
-          return <div key={todo.id}>
-            <h1>{todo.title}</h1>
-            <button onClick={() => deleteTodo(todo.id)}>Done</button>
+      {todos.map(todo => {
+        return (
+          <div key={todo.id}>
+            <div>
+            <input
+                value={todo.title}
+                name="title"
+                onChange={e => updateTodo(e.target.value, todo.id)}
+              ></input>
+              <button onClick={() => deleteTodo(todo.id)}>Done</button>
+            </div>
           </div>
+        );
       })}
     </div>
   );
